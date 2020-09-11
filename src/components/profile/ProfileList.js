9d5582fbc592c1afc33334a2f845e4caadc2581d
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-09-11 18:33:26 -0500

    added a comment reminding myself of what to write for the onClick

--- src/components/profile/ProfileList.js
+++ src/components/profile/ProfileList.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useContext } from "react"
+import React, { useEffect, useContext, useRef } from "react"
 import {EventContext} from "../events/EventsProvider"
+import {ProfileEvents} from "./Profile"
 
 
 
 export const ProfileList = () => {
     const {events, getEvents} = useContext(EventContext)
+    const createEvent = useRef()
 
     useEffect(() => {
         getEvents()
@@ -13,15 +15,26 @@ export const ProfileList = () => {
     return (
         <>
         <h1>Profile Page</h1>
+        <button onClick={() =>{
+            createEvent.current.showModal()
+        }}>create event</button>
+            <dialog className="dialog dialog--createEvent" ref={createEvent}>
+                <p>Enter an Event Title</p>
+                <input type="text" placeholder="type here"></input>
+                <button onClick={() => {
+                    {/* a route/link to send the user to the unique event planning space
+                        also need to send the input to the database 
+                    */}
+                }}>create!</button>
+            </dialog>
         <div>
             {events.map(event => {
-                return <div>{event.name}</div>
+                return <ProfileEvents key={event.id} event={event} />
             })}
         </div>
         </>
     )
 }
-
 
 
 
