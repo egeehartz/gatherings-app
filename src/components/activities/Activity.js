c15705312c692bf325c879eedd8216d7ff0a25a1
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-09-14 18:08:09 -0500

    activities and misc items display with the user that saved the item

--- src/components/activities/Activity.js
+++ src/components/activities/Activity.js
@@ -1,6 +1,15 @@
-import React from "react"
+import React, {useContext, useEffect} from "react"
+import {UserContext} from "../users/UserProvider"
 
 
-export const Activity = (props) => (
-<div>{props.activity.text}</div>
-)
+export const Activity = (props) => {
+    const {users, getUsers} = useContext(UserContext)
+    const userActivity = users.find(u => u.id === props.activity.userId) || {}
+
+    useEffect(() => {
+        getUsers()
+        },[])
+
+    return(
+<div>{props.activity.text} -{userActivity.name}</div>
+)}
--- src/components/misc/Misc.js
+++ src/components/misc/Misc.js
@@ -1,6 +1,14 @@
-import React from "react"
+import React, {useContext, useEffect} from "react"
+import {UserContext} from "../users/UserProvider"
 
 
-export const Misc = (props) => (
-<div>{props.misc.text} </div>
-)
+export const Misc = (props) => {
+    const {users, getUsers} = useContext(UserContext)
+    const userMisc = users.find(u => u.id === props.misc.userId) || {}
+
+    useEffect(() => {
+        getUsers()
+        },[])
+return(
+<div>{props.misc.text} -{userMisc.name}</div>
+)}
