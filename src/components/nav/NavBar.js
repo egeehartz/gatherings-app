7f4e327af7bfa6a8a0dbe171f41022798cf12c8f
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-09-16 13:49:53 -0500

    tried changing to a reactstrap thing without installing react. Reverted back to the way it was originally

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -14,3 +14,31 @@ export const NavBar = (props) => {
         </ul>
     )
 }
+
+// import React, { useState } from 'react';
+// import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+// export const NavBar = (props) => {
+//   const [collapsed, setCollapsed] = useState(true);
+
+//   const toggleNavbar = () => setCollapsed(!collapsed);
+
+//   return (
+//     <div>
+//       <Navbar color="faded" light>
+//         <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
+//         <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+//         <Collapse isOpen={!collapsed} navbar>
+//           <Nav navbar>
+//             <NavItem>
+//               <NavLink href="/components/">Components</NavLink>
+//             </NavItem>
+//             <NavItem>
+//               <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+//             </NavItem>
+//           </Nav>
+//         </Collapse>
+//       </Navbar>
+//     </div>
+//   );
+// }
